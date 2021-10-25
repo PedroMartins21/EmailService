0e920a448347eca11eb328dfb40aa172844d0d9e
PedroMartins21 <49969533+PedroMartins21@users.noreply.github.com>
2021-10-25 18:58:34 -0300

    Delete servico-email directory

--- servico-email/src/main/java/com/example/email/Interfaces/EmailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.email.Interfaces;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.email.model.*;
-public interface EmailRepository extends MongoRepository<EmailModel, String>{
-
-}
--- servico-email/src/main/java/com/example/email/model/StatusEmail.java
+++
@@ -1,6 +0,0 @@
-package com.example.email.model;
-
-public enum StatusEmail {
- send,
- error
-}
